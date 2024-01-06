b71b4226edf99269b2eafbcb5cf99171fcbbe2aa
Panda <connordeleo@gmail.com>
2024-01-06 12:33:40 -0800

    made smol
    SCHMOL

--- main.cpp
+++ main.cpp
@@ -4,31 +4,20 @@
 void centerJustify(int consoleWidth, std::string text, char fill) {
 	int stringLen = text.length();
 	int spaceLen = (consoleWidth - stringLen) / 2;
+	for (int i = 0; i < spaceLen; i++) {
+		std::cout << fill;
+	}
+
+	std::cout << text;
+
+	for (int i = 0; i <= spaceLen; i++) {
+		std::cout << fill;
+	}
 
 	if (stringLen % 2 == 0) {
-		for (int i = 0; i < spaceLen; i++) {
-			std::cout << fill;
-		}
-
-		std::cout << text;
-
-		for (int i = 0; i < spaceLen; i++) {
-			std::cout << fill;
-		}
-
 		std::cout << std::endl;
 	}
 	else {
-		for (int i = 0; i < spaceLen; i++) {
-			std::cout << fill;
-		}
-
-		std::cout << text;
-
-		for (int i = 0; i <= spaceLen; i++) {
-			std::cout << fill;
-		}
-
-		std::cout << std::endl;
+		std::cout << fill << std::endl;
 	}
 }
